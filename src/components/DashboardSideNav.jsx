739adb1d65473b122e0d1a8f7d8834784e528a23
Adisie Fenta <adisiefenta23@gmail.com>
2024-10-17 05:50:28 +0300

    changes

--- src/components/DashboardSideNav.jsx
+++ src/components/DashboardSideNav.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate, NavLink } from "react-router-dom";
 
@@ -14,126 +14,165 @@ import { BsTabletLandscape } from "react-icons/bs";
 import { IoChatbubblesOutline } from "react-icons/io5";
 import { FiUser } from "react-icons/fi";
 
-
 const DashboardSideNav = () => {
-    // states
-    // local states
-    // dashboard links
-    const [dashboardLinks,setDashboardLinks] = useState({
-        links: [
-            {
-                icon: RxDashboard,
-                path: "",
-                text: "Dashboard"
-            },
-            {
-                icon: CiBoxes,
-                subLinks: [
-                    {
-                        icon: IoTvOutline,
-                        path: "",
-                        text: "Television"
-                    },
-                    {
-                        icon: BsTabletLandscape,
-                        path: "",
-                        text: "Tablet"
-                    },
-                    {
-                        icon: IoTvOutline,
-                        path: CiMobile3,
-                        text: "Phone"
-                    },
-                ],
-                text: "Products"
-            },
-            {
-                icon: IoChatbubblesOutline,
-                path: "",
-                text: "Messages"
-            },
-            {
-                icon: FiUser,
-                path: "",
-                text: "Profile"
-            },
+  // states
+  // local states
+  // dashboard links
+  const [dashboardLinks, setDashboardLinks] = useState({
+    links: [
+      {
+        icon: RxDashboard,
+        path: "",
+        text: "Dashboard",
+      },
+      {
+        icon: CiBoxes,
+        subLinks: [
+          {
+            icon: IoTvOutline,
+            path: "",
+            text: "Television",
+          },
+          {
+            icon: BsTabletLandscape,
+            path: "",
+            text: "Tablet",
+          },
+          {
+            icon: IoTvOutline,
+            path: CiMobile3,
+            text: "Phone",
+          },
         ],
-        activeLink: ""
-    })
+        text: "Products",
+      },
+      {
+        icon: IoChatbubblesOutline,
+        path: "",
+        text: "Messages",
+      },
+      {
+        icon: FiUser,
+        path: "",
+        text: "Profile",
+      },
+    ],
+    activeLink: "Dashboard",
+    isOn: false
+  });
   return (
-    <div className="w-[17rem] h-full overflow-hidden rounded-md p-5 bg-neutral-200 flex flex-col justify-between">
+    <div className="w-[17rem] h-full rounded-md p-5 bg-neutral-200 flex flex-col justify-between">
+      <div>
+        {/* header */}
+        <header className="flex items-center justify-between">
+          {/* left */}
           <div>
-            {/* header */}
-            <header className="flex items-center justify-between">
-              {/* left */}
-              <div>
-                <p className="text-sm text-neutral-600">
-                  <span>weather</span>
-                  <span className="font-bold">Forecast</span>
-                </p>
-              </div>
-              {/* right */}
-              <div>
-                <AiOutlineClose className="text-lg text-neutral-500 transition-colors ease-in-out duration-150 hover:text-neutral-700 cursor-pointer" />
-              </div>
-            </header>
-            {/* links */}
-            <div className="mt-7">
-              {
-                dashboardLinks.links.map((linkItem,index)=>{
-                  return (
-                    <div key={linkItem.text} className="mt-3">
-                      {
-                        !linkItem?.subLinks
-                        ?
-                        <NavLink className={'flex items-center  gap-x-2.5 transition-all ease-in-out duration-150 hover:bg-neutral-400 hover:bg-opacity-60 cursor-pointer p-1.5 bg-neutral-300 rounded-sm text-neutral-600'}>
-                          {/* icon */}
-                          <linkItem.icon />
-                          {/* text */}
-                          <div>
-                            <span>{linkItem.text}</span>
-                          </div>
-                        </NavLink>
-                        :
-                        <div className="relative">
-                            {/* header */}
-                            <header className="flex items-center justify-between transition-all ease-in-out duration-150 hover:bg-neutral-400 hover:bg-opacity-60 cursor-pointer p-1.5 bg-neutral-300 rounded-sm text-neutral-600">
-                                {/* left */}
-                                <div className="flex items-center gap-x-2.5">
-                                    {/* icon */}
-                                    <linkItem.icon />
-                                    {/* text */}
-                                    <span>{linkItem.text}</span>
-                                </div>
-                                {/* toggler button */}
-                                <button>
-                                    <AiOutlineClose className={`rotate-45 text-sm text-neutral-700`}/>
-                                </button>
-                            </header>
-                        </div>
-                      }
-                    </div>
-                  )
-                })
-              }
-            </div>
+            <p className="text-sm text-neutral-600">
+              <span>weather</span>
+              <span className="font-bold">Forecast</span>
+            </p>
           </div>
-          {/* footer */}
-          <footer className="flex flex-col gap-y-3">
-            {/* settings */}
-            <button className="w-full flex items-center gap-x-2.5 bg-neutral-300 p-2 rounded-md transition-all ease-in-out duration-150 hover:bg-neutral-400 hover:bg-opacity-65 text-neutral-700">
-              <IoSettingsOutline />
-              <span>Settings</span>
-            </button>
-            {/* logout */}
-            <button className="w-full flex items-center gap-x-2.5 bg-neutral-300 p-2 rounded-md transition-all ease-in-out duration-150 hover:bg-neutral-400 hover:bg-opacity-65 text-neutral-700">
-              <IoLogOutOutline />
-              <span>Logout</span>
-            </button>
-            
-          </footer>
+          {/* right */}
+          <div>
+            <AiOutlineClose className="text-lg text-neutral-500 transition-colors ease-in-out duration-150 hover:text-neutral-700 cursor-pointer" />
+          </div>
+        </header>
+        {/* links */}
+        <div className="mt-7">
+          {dashboardLinks.links.map((linkItem, index) => {
+            return (
+              <div key={linkItem.text} className="mt-3">
+                {!linkItem?.subLinks ? (
+                  <NavLink
+                  onClick={()=>{
+                    setDashboardLinks(prev => {
+                        return {
+                            ...prev,
+                            activeLink: linkItem.text
+                        }
+                    })
+                  }}
+                    className={
+                      "flex items-center  gap-x-2.5 transition-all ease-in-out duration-150 hover:bg-neutral-400 hover:bg-opacity-60 cursor-pointer p-1.5 bg-neutral-300 rounded-sm text-neutral-600"
+                    }
+                  >
+                    {/* icon */}
+                    <linkItem.icon />
+                    {/* text */}
+                    <div>
+                      <span>{linkItem.text}</span>
+                    </div>
+                  </NavLink>
+                ) : (
+                  <div className="relative">
+                    {/* header */}
+                    <header className="flex items-center justify-between transition-all ease-in-out duration-150 hover:bg-neutral-400 hover:bg-opacity-60 cursor-pointer p-1.5 bg-neutral-300 rounded-sm text-neutral-600" 
+                    onClick={()=>{
+                        setDashboardLinks(prev => {
+                            return {
+                                ...prev,
+                                activeLink: linkItem.text,
+                                isOn: !prev?.isOn
+                            }
+                        })
+                      }}
+                    >
+                      {/* left */}
+                      <div className="flex items-center gap-x-2.5">
+                        {/* icon */}
+                        <linkItem.icon  />
+                        {/* text */}
+                        <span>{linkItem.text}</span>
+                      </div>
+                      {/* toggler button */}
+                      <button>
+                        <AiOutlineClose
+                          className={`transition-transform ease-in-out duration-150 text-sm text-neutral-700 ${dashboardLinks.activeLink === linkItem.text && dashboardLinks.isOn ? "rotate-0" : "rotate-45"}`}
+                        />
+                      </button>
+                    </header>
+                    {/* sub links */}
+                    <div className={`transition-transform ease-in-out duration-150 absolute flex flex-col w-max h-max top-1/2 -translate-y-1/2 bg-neutral-300 left-[16.5rem] ${dashboardLinks.activeLink === linkItem.text && dashboardLinks.isOn ? "scale-100" : "scale-0"}`}>
+                      {/* triangle */}
+                      <div className="w-[18px] z-30 aspect-square bg-inherit absolute ml-[-9px] rotate-45 left-0 top-1/2 -translate-y-1/2" />
+                      {linkItem.subLinks.map((subLinkItem) => {
+                        return (
+                          <NavLink
+                            key={subLinkItem.text}
+                            className={
+                              "flex items-center  gap-x-2.5 transition-all ease-in-out duration-150 hover:bg-neutral-400 relative z-50 hover:bg-opacity-60 cursor-pointer p-1.5 bg-neutral-300 rounded-sm text-neutral-600 px-5"
+                            }
+                          >
+                            {/* icon */}
+                            <subLinkItem.icon />
+                            {/* text */}
+                            <span>{subLinkItem.text}</span>
+                          </NavLink>
+                        );
+                      })}
+                    </div>
+                  </div>
+                )}
+              </div>
+            );
+          })}
         </div>
-  )
-}
+      </div>
+      {/* footer */}
+      <footer className="flex flex-col gap-y-3">
+        {/* settings */}
+        <button className="w-full flex items-center gap-x-2.5 bg-neutral-300 p-2 rounded-md transition-all ease-in-out duration-150 hover:bg-neutral-400 hover:bg-opacity-65 text-neutral-700">
+          <IoSettingsOutline />
+          <span>Settings</span>
+        </button>
+        {/* logout */}
+        <button className="w-full flex items-center gap-x-2.5 bg-neutral-300 p-2 rounded-md transition-all ease-in-out duration-150 hover:bg-neutral-400 hover:bg-opacity-65 text-neutral-700">
+          <IoLogOutOutline />
+          <span>Logout</span>
+        </button>
+      </footer>
+    </div>
+  );
+};
 
-export default DashboardSideNav
+export default DashboardSideNav;
